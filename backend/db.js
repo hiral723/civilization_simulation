95fa178a2e6c3136aea27e8f0a62f6ccf322cd19
Hiral Mehta <hiral.mehta24@nmims.in>
2026-04-24 14:31:18 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   port:     process.env.DB_PORT     || 3306,
   database: process.env.DB_NAME     || 'civilization_db',
   user:     process.env.DB_USER     || 'root',
-  password: process.env.DB_PASSWORD || 'Ilovecookies=3',   
+  password: process.env.DB_PASSWORD || '',  //put your password here 
   waitForConnections: true,
   connectionLimit:    10,
   queueLimit:         0,
